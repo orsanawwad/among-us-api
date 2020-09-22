e22a32ea2e21523d92a63eff38a6516611bcfa7a
Nullicorn <bennullicorn@gmail.com>
2020-09-21 23:31:04 -0400

    Begin complete rewrite

---
+++ src/main/java/me/nullicorn/amongus/common/api/Language.java
@@ -0,0 +1,18 @@
+package me.nullicorn.amongus.common.api;
+
+/**
+ * Languages supported for chat & UI in _Among Us_
+ *
+ * @author Nullicorn
+ */
+public enum Language {
+  ENGLISH,
+  SPANISH,
+  KOREAN,
+  RUSSIAN,
+  PORTUGUESE,
+  ARABIC,
+  FILIPINO,
+  POLISH,
+  OTHER
+}
---
+++ src/main/java/me/nullicorn/amongus/common/api/Map.java
@@ -0,0 +1,12 @@
+package me.nullicorn.amongus.common.api;
+
+/**
+ * Playable maps in _Among Us_
+ *
+ * @author Nullicorn
+ */
+public enum Map {
+  THE_SKELD,
+  MIRA_HQ,
+  POLUS
+}
